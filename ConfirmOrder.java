23ad8885c3253c6eadb676680ec15d74615d3796
reddy4617 <90074515+reddy4617@users.noreply.github.com>
2021-10-08 23:24:19 -0500

    Add files via upload

---
+++ ConfirmOrder.java
@@ -0,0 +1,44 @@
+
+public class ConfirmOrder implements BreadState{
+	
+	Bread bread;
+	
+	public ConfirmOrder(Bread bread)
+	{
+		this.bread = bread;
+	}
+	
+	public void receiveOrder()
+	{
+		System.out.println("Order has been recieved ");
+		
+	}
+	public void confirmOrder()
+	{
+		System.out.println("Order is confirming ... ");
+		bread.setState(bread.getProcessOrder());
+	}
+	
+	public void processOrder()
+	{
+		System.out.println("Order is not processing because it is is in confirming state");
+	}
+	public void bakeOrder()
+	{
+		System.out.println("Order is not processed yet... It is in confirming state");
+	}
+	public void readyOrder()
+	{
+		System.out.println("Not ready... It is in confirming state");
+	}
+	public void deliverOrder()
+	{
+		System.out.println("Not ready yet... It is still in confirming state");
+	}
+	
+	public String toString()
+	{
+		return " State ==> Confirm Order ";
+	}
+
+}
---
+++ ReadyOrder.java
@@ -0,0 +1,45 @@
+
+public class ReadyOrder implements BreadState{
+	
+	Bread bread;
+	
+	public ReadyOrder(Bread bread)
+	{
+		this.bread = bread;
+	}
+	
+	public void receiveOrder()
+	{
+		System.out.println("Order has been received already");
+		
+	}
+	public void confirmOrder()
+	{
+		System.out.println("Order is confirmed");
+	}
+	
+	public void processOrder()
+	{
+		System.out.println("Order has been processed");
+	}
+	public void bakeOrder()
+	{
+		System.out.println("Order has been baked");
+	}
+	public void readyOrder()
+	{
+		System.out.println("It is ready and doing packaging...............");
+		bread.setState(bread.getDeliverOrder());
+	}
+	public void deliverOrder()
+	{
+		System.out.println("Order is not delivered yet because packaging needs to be done");
+	}
+	
+	public String toString()
+	{
+		return " State ==> Ready Order ";
+	}
+	
+
+}
